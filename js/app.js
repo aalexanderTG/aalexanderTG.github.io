d7f9f53f63e47fb7e0799f4df62471780c2bdc3e
Aaron Alexander <aalexander@transitionsgroup.net>
2022-06-24 13:51:26 -0500

    feat: multi-photo file selector added

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 var image_input = document.querySelector("#image-input");
+var image_carousel = document.querySelector("#image-carousel");
 image_input.addEventListener("change", function () {
     var reader = new FileReader();
     reader.addEventListener("load", function () {
-        var uploaded_image = reader.result;
-        var elm = document.querySelector("#display-image");
-        elm.style.backgroundImage = "url(".concat(uploaded_image, ")");
+        // Logic needs to go here.
+        var uploaded_image = reader.result; // We have image url now
+        // 1. Create col div
+        var col_div = document.createElement('div');
+        col_div.className = "col-auto mb-3";
+        // 2. Create image div
+        var image_div = document.createElement('div');
+        image_div.className = "img-thumbnail display-image";
+        // 3. Add image to image div
+        image_div.style.backgroundImage = "url(".concat(uploaded_image, ")");
+        // 4. Add image div to col div
+        col_div.appendChild(image_div);
+        // 5. Add col div to image-carousel row div
+        image_carousel.appendChild(col_div);
     });
     reader.readAsDataURL(this.files[0]);
 });
--- js/app.ts
+++ js/app.ts
@@ -1,11 +1,28 @@
 const image_input = document.querySelector("#image-input");
+const image_carousel = document.querySelector("#image-carousel") as HTMLDivElement;
 
 image_input!.addEventListener("change", function() {
   const reader = new FileReader();
   reader.addEventListener("load", () => {
-    const uploaded_image = reader.result;
-    var elm: HTMLElement = document.querySelector("#display-image")!;
-    elm.style.backgroundImage = `url(${uploaded_image})`;
+    // Logic needs to go here.
+    const uploaded_image = reader.result; // We have image url now
+
+    // 1. Create col div
+    const col_div = document.createElement('div') as HTMLDivElement;
+    col_div.className = "col-auto mb-3";
+
+    // 2. Create image div
+    const image_div = document.createElement('div') as HTMLDivElement;
+    image_div.className = "img-thumbnail display-image";
+
+    // 3. Add image to image div
+    image_div.style.backgroundImage = `url(${uploaded_image})`;
+
+    // 4. Add image div to col div
+    col_div.appendChild(image_div);
+
+    // 5. Add col div to image-carousel row div
+    image_carousel.appendChild(col_div);
   });
   reader.readAsDataURL(this.files[0]);
 });
