47f91f901988d4db0ab9e5d0d128f898c7e5edd0
Aaron Alexander <aalexander@transitionsgroup.net>
2022-06-24 08:29:12 -0500

    moved project to typescript

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-const image_input = document.querySelector("#image-input");
-
-image_input.addEventListener("change", function() {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => {
-    const uploaded_image = reader.result;
-    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
-  });
-  reader.readAsDataURL(this.files[0]);
+var image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function () {
+    var reader = new FileReader();
+    reader.addEventListener("load", function () {
+        var uploaded_image = reader.result;
+        var elm = document.querySelector("#display-image");
+        elm.style.backgroundImage = "url(".concat(uploaded_image, ")");
+    });
+    reader.readAsDataURL(this.files[0]);
 });
